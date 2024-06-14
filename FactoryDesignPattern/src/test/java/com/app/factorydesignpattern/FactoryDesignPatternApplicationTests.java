2e3663911d2e8a1c8099882e835225a2b66c5015
Iftekharuddin Syed <syedy636@gmail.com>
2024-06-14 16:50:16 +0200

    Adding module for FactoryDesignPattern

---
+++ FactoryDesignPattern/src/test/java/com/app/factorydesignpattern/FactoryDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.factorydesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
